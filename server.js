a8926e1f79074d5b0967df681103e31f1d0a3e5a
billmcconnell <44976673+billmcconnell@users.noreply.github.com>
2023-12-10 19:32:40 -0500

    added production path logic to server.js

--- server.js
+++ server.js
@@ -153,6 +153,17 @@ app.get('/account/all', function (req, res) {
         });
 });
 
+// serve static files from React app if in production
+if (process.env.NODE_ENV === 'production') {
+    // set static folder
+    app.use(express.static('client/build'));
+    // app.use(express.static(path.join(__dirname, 'client', 'build')));
+    
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+    }
+
 const PORT = process.env.PORT || 5500;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
